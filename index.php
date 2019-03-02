915c1e269c20d610f6bdf1f45ce640a8cf75e574
Genessis <ggalindo@u.rochester.edu>
2019-03-01 22:48:09 -0500

    Fix

--- index.php
+++ index.php
@@ -1,70 +1,56 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
 	<?php include "./includes/head.php"; ?>
-	<title>Assignment 5: Odd Student Out | Students</title>
-	
+	<title>Assignment 5: Odd Student Out</title>
+	<link rel="stylesheet" href="css/lgallery.min.css">
 </head>
-
 <body class="container">
-	<?php include "./includes/nav.php"; ?>
-	<main>
+	<?php include "includes/nav.php"; ?>
 
-		<section class="section-grid">
-			<div>
-				<h2 class="russo green">Akshay Desi Computer Science</h2>
-				<a href="students.php"><img class="img-shadow" src="images/akshay.jpg" alt="Akshay Desi"></a>
-			</div>
+	
+	<!-- Full Width Slideshow -->
+	<div class="slideshow-box" data-lgslider="./images/change.png, ./images/won.png, ./images/glider.jpg " 
+		data-lgnoscale="true"
+		data-lgrender="true"
+		data-lgnobtns = "true"
+		data-lglabel="Akshay, Won, Tom"></div>
 
-			<article>
-				<p>I took many interesting courses in computer science. I started from the beginning, taking “The Science of Programming”, The University of Rochester’s introduction class to computer science. Since then, I’ve taken many
-				classes and even took more classes than were required for the major. A few notable ones are “Operating Systems”, Advanced Algorithms”, “Web Development”, and “Android </p>
-				</article>
-				<article>
-				<p>Development”; in these classes, I’ve learned many concepts and techniques important to being a software engineer. I also have been a Workshop Leader/ Teaching Assistant beginning my second semester of college. I led workshops for “The Science of Programming” and then “Data Structures and Algorithms”. </p>
-				</article>
+
+	<section class="article-grid">
+		<section>
+			<h2>Akshay Desai</h2>
+			<p>I am a senior at the University of Rochester and majoring in Computer Science. I will be graduating this
+				Spring and will be moving out to Seattle shortly after. On campus, I am also a Residential Advisor.</p>
+		</section>
+	
+		<section>
+			<h2>Won Kuk Lee</h2>
+			<p>I am currently a senior at the University of Rochester. I’m majoring in Computer Science and plan to graduate
+				this May with a Bachelor of Science. I have a passion for transforming innovative ideas to seamless and
+				elegant web applications.</p>
+		</section>
+	
+		<section>
+			<h2>Tom Hogrefe</h2>
+			<p>I’m a senior at the University of Rochester studying Political Science with a minor in Computer Science.
+				Outside of class, I’m a member of Sigma Phi Epsilon and a Speaking Fellow. I enjoy aviation and programming
+				in my spare time.</p>
 		</section>
 
-		<section class="section-grid">
-			<div>
-				<h2 class="russo green">Won Kuk Lee Computer Science</h2>
-				<a href="students.php"><img class="img-shadow" src="images/won.jpg" alt="Won Kuk Lee"></a>
+		<a href="students.php" class="myButton">
+			<div>Read</div>
+			<div>More</div>
+		</a>
+	</section>
 
-			</div>
+	<?php include "includes/footer.php"; ?>
 
+	<?php include "includes/scripts.php"; ?>
+	<script src="js/lgallery.min.js"></script>
+	<script >
+		initLG()
+	</script>
 
-			<article>
-				<p>I’m majoring in Computer Science and plan to graduate this May with a Bachelor of Science. I thought that the BS program in the University of Rochester is quite challenging as I struggled through
-				many of the core courses, most notably CSC 282 and 252. However, I have fortunately gotten through all of
-				them and only need one more 200 level course to graduate  </p>
-			</article>
-			<article>
-				<p>and have decided to take CSC 261 as this final course. For my advanced CS course electives, I decided to concentrate on web and mobile programming as I’ve
-				taken CSC 210 and 214. In addition to these courses, I have also taken CSC 170, CSC 172, and currently taking CSC 174.</p>
-			</article>
-		</section>
-
-		<section class="section-grid" id="odd-one">
-			<div>
-				<h2 class="play-fair red">Tom Hogrefe Political Science</h2>
-				<a href="students.php"><img class="img-shadow" src="images/tom.jpg" alt="Tom Hogrefe"></a>
-
-			</div>
-
-
-			<article>
-				<p>I’ve taken a lot of courses (almost enough to graduate!) while at the U of R. One of my favorites was my first political science course, Game Theory. The course certainly wasn’t fun and games, but it did include a lot of interesting concepts such as the Prisoner’s Dilemma and other formalizations of problems. The entire course was focused on how decisions should be made when those decisions are dependent on other parties’ </p>
-			</article>
-			<article>
-				<p>decisions. Another interesting course I took was called Research Practicum in Criminal Justice Reform. This course was much easier than Game Theory, but I learned a lot of practical knowledge from it. The course was partnered with a non-profit called Measures for Justice, and students were able to gain experience reading legal statutes and gathering data -- this would eventually be used by MFJ as part of their database intended to showcase data and abnormalities across county-level criminal justice systems.
-				</p>
-			</article>
-		</section>
-	</main>
-
-	<?php include "./includes/footer.php"; ?>
-	<?php include "./includes/scripts.php"; ?>
 </body>
-
 </html>
